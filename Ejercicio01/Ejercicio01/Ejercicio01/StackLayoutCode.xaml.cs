1f549b191534bc90b2f1efc10e06ddb883df6439
Vargas-Cris <cristhina.vargas@tecsup.edu.pe>
2022-04-10 16:21:23 -0500

    Laboratorio 03

---
+++ Ejercicio01/Ejercicio01/Ejercicio01/StackLayoutCode.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutCode : ContentPage
+    {
+        public StackLayoutCode()
+        {
+            InitializeComponent();
+            var layout = new StackLayout();
+            var button = new Button
+            {
+                Text = "StackLayout",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            var yellowBox = new BoxView
+            {
+                Color = Color.Yellow,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            var greenBox = new BoxView
+            {
+                Color = Color.Green,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            var blueBox = new BoxView
+            {
+                Color = Color.Blue,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            layout.Children.Add(button);
+            layout.Children.Add(yellowBox);
+            layout.Children.Add(greenBox);
+            layout.Children.Add(blueBox);
+            layout.Spacing = 10;
+            Content = layout;
+        }
+    }
+}
