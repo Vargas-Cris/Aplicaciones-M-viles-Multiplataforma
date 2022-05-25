fe6dac6604aa93033049ea863d60766435923966
Vargas-Cris <cristhina.vargas@tecsup.edu.pe>
2022-05-25 17:05:32 -0500

    Laboratorio 10

---
+++ Lab10/Lab10/Lab10/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab10
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            // Menu buttons
+            item1.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+            item2.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            item3.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+            item4.Clicked += async (object sender, EventArgs e) =>
+            {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+        }
+
+    }
+}
