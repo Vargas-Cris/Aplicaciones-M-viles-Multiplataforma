f6405c89107fa947ed1e0d2f8304ca07437ac193
Vargas-Cris <cristhina.vargas@tecsup.edu.pe>
2022-06-11 12:19:59 -0500

    Laboratorio 12

---
+++ Lab12/Lab12/Lab12/Views/Calculadora.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab12.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calculadora : ContentPage
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+            this.BindingContext = new Lab12.ViewModels.CalculadoraViewModel();
+        }
+    }
+}
