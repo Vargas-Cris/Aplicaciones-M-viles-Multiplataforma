3e16140c4c32e2496ded079cafd2dffacc0150a5
Vargas-Cris <cristhina.vargas@tecsup.edu.pe>
2022-06-16 22:14:10 -0500

    Laboratorio 13

---
+++ Lab13/Lab13/Lab13.iOS/Implementations/ConfigDataBase.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Lab13.Interfaces;
+using Lab13.iOS.Implementations;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace Lab13.iOS.Implementations
+{
+    public class ConfigDataBase : IConfigDataBase
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseFileName);
+        }
+    }
+}
+
